6052acf8e7d2992a3e6ceed3ff33e6c95f835be9
Louis <louis.zerri@hotmail.com>
2025-12-15 01:26:42 +0100

    feat: add Montserrat font link for improved typography

--- resources/views/minisite/home.blade.php
+++ resources/views/minisite/home.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
     <style>
         body {
             font-family: 'Open Sans', sans-serif;
